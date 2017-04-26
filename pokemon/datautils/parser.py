584a876d17215ae91f16144b8c7aff63de9b0091
Brian Bao <btb79@cornell.edu>
2017-04-25 21:59:41 -0400

    Fix parser

--- pokemon/datautils/parser.py
+++ pokemon/datautils/parser.py
@@ -14,18 +14,17 @@ class PokemonShowdownReplayParser(object):
 		self.parse()
 
 		output = ""
-		output += this.players["p1"].getTeamFormatString()
-		output += this.players["p2"].getTeamFormatString()
+		output += self.players["p1"].getTeamFormatString()
+		output += self.players["p2"].getTeamFormatString()
 
 		return output
 
 	def parse(self):
 		lines = self.log.split('\n')
-		print(lines)
 		for line in lines:
 			if line.startswith("|player|"):
 				self.processPlayer(line)
-			elif line.startswith("|poke|"): 
+			elif line.startswith("|poke|"):
 				self.processPoke(line)
 			elif line.startswith("|move|"):
 				self.processMove(line)
@@ -33,18 +32,18 @@ class PokemonShowdownReplayParser(object):
 				self.processAbility(line)
 			elif line.startswith("|switch|"):
 				self.processSwitch(line)
-			elif line.startswith("|drag|"):
-				self.processDrag(line)
+			# elif line.startswith("|drag|"):
+			# 	self.processDrag(line)
 			elif line.startswith("|-mega|"):
 				self.processMega(line)
 			elif line.startswith("|detailschange|"):
 				self.processDetailsChange(line)
 			# elif line.startswith("|-item|"):
-			# 
+			#
 			#		self.processItem(line)
-			#		 			
+			#
 			elif line.startswith("|-enditem|"):
-				self.processEndItem(line) 
+				self.processEndItem(line)
 			elif "|[from] move:" in line:
 				if line.startswith("|-item|"):
 					self.processItemFromMove(line)
@@ -53,9 +52,9 @@ class PokemonShowdownReplayParser(object):
 					self.processHealFromItem(line)
 			elif "[from] ability: " in line:
 				if line.startswith("|-weather|"):
-					self.processWeatherFromAbility(line)	
+					self.processWeatherFromAbility(line)
 			else:
-				if (line.startswith("|J|") 
+				if (line.startswith("|J|")
 					or line.startswith("|j|")
 					or line.startswith("|L|")
 					or line.startswith("|l|")
@@ -101,120 +100,117 @@ class PokemonShowdownReplayParser(object):
 
 		if len(fields) >= 4:
 			self.players[fields[2]].username = fields[3]
-	
+
 
 	def processPoke(self, line):
 		fields = line.split("|")
 
 		pokemon = Pokemon()
 		pokemon.species = fields[3].replace("/,.*$/", "")
-		self.players[fields[2]].pokemon.push(pokemon)
-	
+		self.players[fields[2]].pokemon.append(pokemon)
+
 
 	def processSwitch(self, line):
-		print(line)
-		matches = re.search("/\|switch\|(p[12])a:\s+([^|]+)\|([^,|]+)/", line)
-		print(matches)
-		player = matches[1]
-		nickname = matches[2]
-		species = matches[3]
+		matches = re.search("\|switch\|(p[12])a:\s+([^|]+)\|([^,|]+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		species = matches[2]
 
 		print("###"+line)
 		print(self.players[player])
-		pokemon = self.players[player].getPokemonBySpecies(species)	
-		if pokemon == null:
+		pokemon = self.players[player].getPokemonBySpecies(species)
+		if pokemon == None:
 			pokemon = Pokemon()
 			pokemon.species = species
-			self.players[player].pokemon.push(pokemon)
+			self.players[player].pokemon.append(pokemon)
 		pokemon.nickname = nickname
 		self.players[player].currentPokemon = pokemon
 
 
 	def processMove(self, line):
-	
-		matches = re.search("/\|move\|(p[12])a:\s+([^|]+)\|([^|]+)/", line)
-		player = matches[1]
-		nickname = matches[2]
-		move = matches[3]
+		matches = re.search("\|move\|(p[12])a:\s+([^|]+)\|([^|]+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		move = matches[2]
 
 		pokemon = self.players[player].getPokemonByNickname(nickname)
 		pokemon.moves[move] = 1
-	
+
 
 	def processAbility(self, line):
-		matches = re.search("/\|-ability\|(p[12])a:\s+([^|]+)\|([^|]+)/", line)	
-		player = matches[1]
-		nickname = matches[2]
-		ability = matches[3]
+		matches = re.search("\|-ability\|(p[12])a:\s+([^|]+)\|([^|]+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		ability = matches[2]
 
 		pokemon = self.players[player].getPokemonByNickname(nickname)
 		pokemon.ability = ability
-	
+
 
 	def processMega(self, line):
-		matches = re.search("/\|-mega\|(p[12])a:\s+([^|]+)\|([^|]+)\|(.+)/", line)
-		player = matches[1]
-		nickname = matches[2]
-		# var species = matches[3]
-		megastone = matches[4]
+		matches = re.search("\|-mega\|(p[12])a:\s+([^|]+)\|([^|]+)\|(.+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		# var species = matches[2]
+		megastone = matches[3]
 
 		pokemon = self.players[player].getPokemonByNickname(nickname)
 		pokemon.item = megastone
-	
+
 
 	def processDetailsChange(self, line):
-		matches = re.search("/\|detailschange\|(p[12])a:\s+([^|]+)\|([^,]+)/", line)
-		player = matches[1]
-		nickname = matches[2]
-		species = matches[3]
+		matches = re.search("\|detailschange\|(p[12])a:\s+([^|]+)\|([^,]+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		species = matches[2]
 
 		pokemon = self.players[player].getPokemonByNickname(nickname)
 		pokemon.species = species
-	
-	
+
+
 	def processItemFromMove(self, line):
-		matches = re.search("/\|-item\|(p[12])a:\s+([^|]+)\|([^|]+)/", line)
-		player = matches[1]
-		nickname = matches[2]
-		item = matches[3]
+		matches = re.search("\|-item\|(p[12])a:\s+([^|]+)\|([^|]+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		item = matches[2]
 
 		otherPlayer = "p2" if player == "p1" else "p1"
 		otherPokemon = self.players[otherPlayer].currentPokemon
 
 		if otherPokemon.item == "":
 			otherPokemon.item = item
-	
+
 
 	def processEndItem(self, line):
-		matches = re.search("/\|-enditem\|(p[12])a:\s+([^|]+)\|([^|]+)/", line)
-		player = matches[1]
-		nickname = matches[2]
-		item = matches[3]
+		matches = re.search("\|-enditem\|(p[12])a:\s+([^|]+)\|([^|]+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		item = matches[2]
 
 		pokemon = self.players[player].getPokemonByNickname(nickname)
 		if pokemon.item == "":
 			pokemon.item = item
-	
+
 
 	def processHealFromItem(self, line):
-		matches = re.search("/-heal\|(p[12])a:\s+([^|]+)\|[^|]+\|\[from\] item: (.+)/", line)
-		player = matches[1]
-		nickname = matches[2]
-		item = matches[3]
+		matches = re.search("-heal\|(p[12])a:\s+([^|]+)\|[^|]+\|\[from\] item: (.+)", line).groups()
+		player = matches[0]
+		nickname = matches[1]
+		item = matches[2]
 
 		pokemon = self.players[player].getPokemonByNickname(nickname)
 		pokemon.item = item
-	
+
 
 	def processWeatherFromAbility(self, line):
-		matches = re.search("/\|-weather\|[^|]+\|\[from\] ability: ([^|]+)\|\[of\] (p[12])a: (.+)/", line)
-		ability = matches[1]
-		player = matches[2]
-		nickname = matches[3]
-		
+		matches = re.search("\|-weather\|[^|]+\|\[from\] ability: ([^|]+)\|\[of\] (p[12])a: (.+)", line).groups()
+		ability = matches[0]
+		player = matches[1]
+		nickname = matches[2]
+
 		pokemon = self.players[player].getPokemonByNickname(nickname)
 		pokemon.ability = ability
-	
+
 
 
 class Player(object):
@@ -272,5 +268,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-	
-
