f77682611822ad52c4ae7235633ee50f9e6b2600
Brian Bao <btb79@cornell.edu>
2017-05-06 22:32:30 -0400

    Base

--- pokemon/game/pokemonenv.py
+++ pokemon/game/pokemonenv.py
@@ -19,9 +19,41 @@ class PokemonShowdownConfigSelfPlay(object):
 	learner=ApproxQLearner
 
 class PokemonShowdown(Environment):
-
-	def __init__(self, config):
+	def __init__(self, config, driver):
 		self.learner = config.learner
+		self.driver = driver
+
+	def getCurrentState(self):
+		'''
+		Gets the current state of the environment.
+		'''
+		raise NotImplementedError('Environment should implement this')
+
+	def getActions(self, state=None):
+		'''
+		Returns the actions possible for the current state. If state=None, return all actions.
+		'''
+		pass
+
+	def reset(self):
+		'''
+		Resets the environment to the start state
+		'''
+		raise NotImplementedError('Environment should implement this')
+
+	def update(self, action):
+		'''
+		Updates the state based upon the current state and action. Returns the new state
+		and a reward
+		'''
+		raise NotImplementedError('Environment should implement this')
+
+	def isEndState(self):
+		'''
+		Checks if it is an end state, e.g. the episode is over
+		'''
+		raise NotImplementedError('Environment should implement this')
+
 
 DRIVERFOLDER = 'driver/chromedriver.exe'
 BASEURL = 'http://play.pokemonshowdown.com/'
@@ -65,4 +97,3 @@ def runAgainstItself():
 	login(driver2, showdown_config2)
 	challenge(driver1, driver2, showdown_config1, showdown_config2)
 
-	
